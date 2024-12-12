16e3160146207df2b6d24fc045e3a8da7b68bfa0
Olga <valor.wuolg24@triana.salesianos.edu>
2024-12-12 14:31:01 +0100

    monumentosv2

---
+++ monumentosv2/monumentosv2/src/main/java/com/salesianostriana/monumentosv2/Monumentosv2Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.monumentosv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Monumentosv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Monumentosv2Application.class, args);
+	}
+
+}
